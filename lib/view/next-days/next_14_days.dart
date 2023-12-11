8c1da25a46429156e52780638aa0144c86ea43d7
prince089 <prince.hirapara890@gmail.com>
2023-12-11 21:29:57 +0530

    next 14 day screen complete.

--- lib/view/next-days/next_14_days.dart
+++ lib/view/next-days/next_14_days.dart
@@ -19,9 +19,84 @@ class NextDays extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SizedBox(
-          height: size.height,
-          width: size.width,
-          child: Stack(
+            height: size.height,
+            width: size.width,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  expandedHeight: 220,
+                  // floating: false,
+                  // pinned: true,
+                  flexibleSpace: Container(
+                    height: size.height / 1.8,
+                    width: size.width,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40),
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Color(0xff6ac5fe),
+                          Color(0xff45b6fe),
+                          Color(0xff45b6fe),
+                          // Colors.blue
+                        ],
+                      ),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const CustomAppBar(),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          DaysList(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Container(
+                        decoration: const BoxDecoration(
+                          // color: Colors.blue,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15.0),
+                            bottomRight: Radius.circular(15.0),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Hero(
+                              tag: 'TAG',
+                              child: Material(
+                                  color: Colors.transparent,
+                                  child: CenterCard()),
+                            ),
+                            BottomList(),
+                          ],
+                        ),
+                      )
+                      //DaysList(),
+                    ],
+                  ),
+                ),
+              ],
+            )
+
+            /*Stack(
             // alignment: Alignment.center,
             children: [
               Positioned(
@@ -65,8 +140,8 @@ class NextDays extends StatelessWidget {
                 ),
               )
             ],
-          ),
-        ),
+          ),*/
+            ),
       ),
     );
   }
